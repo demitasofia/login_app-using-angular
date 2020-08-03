224863ffdc4626df20143fc1a5f3fec9fca0ae2d
demita sofia <32023361+demitasofia@users.noreply.github.com>
2020-08-03 16:39:24 +0530

    sign and user details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,17 @@
-/*import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'Abide-login';
-}
-
-import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
-})
-export class AppComponent {
-  items: Observable<any[]>;
-  constructor(firestore: AngularFirestore) {
-    this.items = firestore.collection('items').valueChanges();
-  }
-}*/
-
-import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
 
 @Component({
   selector: 'app-root',
@@ -35,24 +19,95 @@ import { auth } from 'firebase/app';
   styleUrls: ['./app.component.css']
  
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  email:string;
+  password:string;
+  returnedArray = [];
+  loggedUser: string;
+  $user: Observable<User>
+  userEmail: string;
 
-  constructor(public auth: AngularFireAuth) {
+
+  constructor(public auth: AngularFireAuth,
+    private afs: AngularFirestore
+    ){this.$user = this.auth.authState.pipe(
+      switchMap(user => {
+        if(user) {
+          return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+        } else {
+          return of(null);
+        }
+      })
+    );}
+         
+     
+  
+  async login() {
+     const credentials = await this.auth.signInWithPopup(new auth.FacebookAuthProvider())
+     console.log(credentials.user.email);
+    this.userEmail = credentials.user.email;
+    console.log('User Email :' + this.userEmail);  
   }
-  login() {
-    this.auth.signInWithPopup(new auth.FacebookAuthProvider());
-    // if this condtion is success
-    // profile page
-  }
-  logout() {
-    this.auth.signOut();
+  async logout() {
+    await this.auth.signOut();
     
     //  login panna hello kela logout
-    
-  }
- 
+    }
+
+ signup(){
+   this.auth.createUserWithEmailAndPassword(this.email,this.password)
+   .then((userCredentials) => console.log(userCredentials));
+   
+ }
+ ngOnInit(){
+  this.db.collection('users').snapshotChanges().subscribe(
+    serviceItems => {
+      this.returnedArray = [];
+      serviceItems.forEach(a=>{
+        let item:any = a.payload.doc.data();
+        item.id = a.payload.doc.id;
+        this.returnedArray.push(item);
+      })
+    }
+  )
+
+  this.loggedUser = this.auth.userEmail;
+}
+
+getUserEmail(){
+  return this.userEmail;
+}
+
+getCurrentUser() {
+  console.log(this.auth.getUserEmail());
   
-  }
+}
+
+/* var user = firebase.auth().currentUser;
+
+if (user != null) {
+  user.providerData.forEach(function (profile) {
+    console.log("Sign-in provider: " + profile.providerId);
+    console.log("  Provider-specific UID: " + profile.uid);
+    console.log("  Name: " + profile.displayName);
+    console.log("  Email: " + profile.email);
+    console.log("  Photo URL: " + profile.photoURL);
+  });*/
+}
+
+export interface User {
+  uid: string;
+  email: string;
+}
+
+
+
+
+
+
+  
+  
     
  
+
 
